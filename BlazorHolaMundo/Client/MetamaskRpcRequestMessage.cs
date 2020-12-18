31bbf93c846c67f77528bb4acc91b07d5c4cfb9c
jeisson niño <jeissoni_22@hotmail.com>
2020-12-18 18:51:08 -0500

    first commit

---
+++ BlazorHolaMundo/Client/MetamaskRpcRequestMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nethereum.JsonRpc.Client.RpcMessages;
+using Newtonsoft.Json;
+
+namespace BlazorHolaMundo.Client
+{
+    public class MetamaskRpcRequestMessage : RpcRequestMessage
+    {
+
+        public MetamaskRpcRequestMessage(object id, string method, string from, params object[] parameterList) : base(id, method,
+            parameterList)
+        {
+            From = from;
+        }
+
+        [JsonProperty("from")]
+        public string From { get; private set; }
+    }
+}
---
+++ BlazorHolaMundo/Client/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BlazorHolaMundo.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+
+            builder.Services.AddSingleton<IMetamaskInterop, MetamaskBlazorInterop>();
+            builder.Services.AddSingleton<MetamaskService>();
+            builder.Services.AddSingleton<MetamaskInterceptor>();
+
+
+        }
+
+    }
+}
